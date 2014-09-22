b634292666df8607eb3e33514658fd6957a9bb57
Joe Ryan <joe@clanryan.us>
2014-09-21 21:00:09 -0400

    add code raising exceptions for index out of bounds

--- Percolation.java
+++ Percolation.java
@@ -1,13 +1,20 @@
 /**
  * Percolation.java - class for an N-by-N grid that can test for percolation
  * Joe Ryan, 23 July 2014
- * Creates an N-by-N grid
- * open(row, column) allows opening of elements
- * isOpen(row, column) tests the state of element at (row, column)
- *    and returns true if open
- * isFull(row, column) tests the ??
+ * Dependencies: stdlib.jar and algs4.jar (libraries provided by Algorithms class
+ * Command Line usage: java Percolation <size of grid N>
+ * Constructor creates an N-by-N grid using a single WeightedQuickUnionUF object
+ *    initialized with all elements closed
+ * main() reads two entries from stdin while available, opens element at i,j
+ *    and when no more entries are available prints //TODO output 
+ * .open(row, column) allows opening of a single element, any surrounding element
+ *    which is opened is then connected
+ * isOpen(row, column) tests the state of an element at (row, column)
+ *    and returns true if it is open
+ * isFull(row, column) tests the element to see if it can connect to the top
+ *    pseudo row indicating any element in the top row
  * percolates() tests system for a path from the bottom row all the way through
- * to the top row and returns true if a valid path exists from row 1 to row N
+ *    to the top row and returns true if a valid path exists from row 1 to row N
  */
 
 public class Percolation {
@@ -20,6 +27,10 @@ public class Percolation {
      * the integer N must be > 0 and determines the size of the grid
      * @param N specifies the size of the square grid to create
      */
+        if (N < 0) {
+            throw new IndexOutOfBoundsException(
+                "Initial size of system must be equal to or greater than 1");
+        }
         grid = new WeightedQuickUnionUF [N*N];
         state = new boolean [N*N];
         size = N;
@@ -28,6 +39,10 @@ public class Percolation {
     public void open(int i, int j) {  /**
      * opens the site (row i, column j) if it not already open
      */
+        if (i <= 0 || i > size) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > size) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");
         if (!isOpen(i, j)) { state[i*size+j] = true; }
     }
     
@@ -37,7 +52,10 @@ public class Percolation {
      * @param i row for site being checked
      * @param j column for site being checked
      */
-       
+        if (i <= 0 || i > size) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > size) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");       
         return state[i*size+j];
        
     }
@@ -70,6 +88,3 @@ public class Percolation {
         }
     }
 }
-    
-    
-                         
